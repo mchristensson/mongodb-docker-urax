d80066fae76edba34cf0e9ac3f4e961b631c989d
Magnus Christensson <magnus.christensson@hotmail.se>
2023-03-07 15:24:36 +0100

    wip

---
+++ src/main/java/org/mac/urax/UraxApplication.java
@@ -0,0 +1,13 @@
+package org.mac.urax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UraxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UraxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mac/urax/UraxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mac.urax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UraxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
